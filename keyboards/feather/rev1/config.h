db25c8658f4905f7ef4b6304cee3085a0571627c
tpywao <tpywao@gmail.com>
2021-10-31 18:42:00 +0900

    Fix ディレクトリ変更

--- keyboards/feather/rev1/config.h
+++ keyboards/feather/rev1/config.h
@@ -44,7 +44,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // A~F, 1~4
 #define MATRIX_ROW_PINS { F6, F7, B1, B3 }
 #define MATRIX_COL_PINS { D1, D0, D4, C6, D7, E6 }
-#define UNUSED_PINS
+// #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
 #define DIODE_DIRECTION COL2ROW
